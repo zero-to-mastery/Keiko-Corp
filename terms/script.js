8f8f533dfaa16c2345b395c9b756f4929bbed6c1
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-10-01 19:47:40 +0000

    (CI) format code

--- terms/script.js
+++ terms/script.js
@@ -1,25 +1,25 @@
 // Get the modal
-var modal = document.getElementById("modalBox");
+var modal = document.getElementById('modalBox');
 
 // Get the button that opens the modal
-var btn = document.getElementById("openModal");
+var btn = document.getElementById('openModal');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+span.onclick = function () {
+  modal.style.display = 'none';
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
