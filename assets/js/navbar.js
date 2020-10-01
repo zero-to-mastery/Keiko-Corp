54c22276c985936af4ba33029dc6af12e6ec8991
Leonardo H Vieira <leohvir@gmail.com>
2020-10-01 14:11:55 -0300

    adding Navbar

---
+++ assets/js/navbar.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.nav-button');
+
+function openMenu() {
+    const menu = document.querySelector('.menu');
+
+    menu.classList.toggle('open');
+}
+
+button.addEventListener("click", openMenu);
