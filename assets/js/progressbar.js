17b031d53dd848f38207aeb54833ef65845f8140
Jappanjeet Singh <jappanjeet.99@gmail.com>
2020-10-02 20:42:46 +0530

    adding progress bar feature

---
+++ assets/js/progressbar.js
@@ -0,0 +1,33 @@
+// declaring variables
+const scrollLine = document.querySelector('.scroll-line');
+
+// define functions
+function fillScrollLine() {
+    const windowHeight = window.innerHeight;
+    const fullHeight = document.body.clientHeight;
+    const scrollY = window.scrollY;
+    const scrolled = (scrollY / (fullHeight - windowHeight)) * 100;
+    scrollLine.style.width = scrolled + '%';
+    console.log(windowHeight, fullHeight, scrollY, scrolled);
+    console.count('Hello!');
+}
+
+function debounce(func, wait = 15, immediate) {
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+// event listeners here
+window.addEventListener('scroll', debounce(fillScrollLine));
+
