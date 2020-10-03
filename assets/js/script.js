de28dd346e3da5368ae578249678dfca3bc5a40b
carmen iancu <carmen.iancu.mail@gmail.com>
2020-10-03 13:42:31 +0300

    fixed scroll button position

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let currentScrollY = 0;
+let showScrollButton = false;
+
+function onScroll() {
+    currentScrollY = window.pageYOffset;
+    showScrollButton = Boolean(currentScrollY > 1);
+
+    showScrollButton ? scrollButton.classList.remove('scroll-to-top-wrapper--hide'): 
+    scrollButton.classList.add('scroll-to-top-wrapper--hide');
+}
+
+window.onload = function() {
+    main = document.getElementById('main');
+    scrollButton = document.getElementById('scroll-to-top-wrapper');
+    document.addEventListener('scroll', onScroll, false)
+  }
