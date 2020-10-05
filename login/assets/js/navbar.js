ca9844ea0ff62def11dd9e4f06a8a01f87348e8c
comebackid87 <vieiromeister05@gmail.com>
2020-10-05 15:27:21 +0100

    updated login task

--- login/assets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('.nav-button');
-
-function openMenu() {
-  const menu = document.querySelector('.menu');
-  menu.classList.toggle('open');
-}
-
-button.addEventListener('click', openMenu);
